dd029b5b30e7b6ed42d7886c7f2f5ac1082a78fd
Topaz712 <tzam712@gmail.com>
2023-09-25 01:47:02 -0500

    exercise two done

---
+++ Exercise-2/arrays-and-tuples.ts
@@ -0,0 +1,8 @@
+//Declare an array named subjects containing strings.
+let subjects: string[];
+
+//Declare a tuple named student that contains a string (for the name) and a number (for the age).
+let student: {
+  name: string;
+  age: number;
+};
