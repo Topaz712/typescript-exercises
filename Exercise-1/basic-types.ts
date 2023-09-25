9c692241a8b5930c45dadf0d5b7054f04829221e
Topaz712 <tzam712@gmail.com>
2023-09-25 01:42:16 -0500

    exercise one

---
+++ Exercise-1/basic-types.ts
@@ -0,0 +1,11 @@
+//Declare a variable studentName of type string and assign it your name.
+let studentName: string;
+
+studentName = "Topacio";
+console.log(studentName);
+//Declare a variable totalCourses of type number and assign it the number of courses you are taking.
+
+let totalCourses: number;
+totalCourses = 1;
+console.log(totalCourses);
+//Log these variables to the console.
