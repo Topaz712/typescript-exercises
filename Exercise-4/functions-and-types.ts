50fa7726d4fe764f5e7e35b2fd519f3cfd90a0c1
Topaz712 <tzam712@gmail.com>
2023-09-25 02:10:48 -0500

    exercise 4: functions and types done

---
+++ Exercise-4/functions-and-types.ts
@@ -0,0 +1,13 @@
+//Write a function greet that takes in a name (string) and returns a greeting (string).
+
+function greet(name: string) {
+  return `Hello, ${name}`;
+}
+
+greet("Topacio");//Hello, Topacio
+
+/* can also be set like so:
+
+const greeting = greet("Topacio");
+console.log(greeting);//Hello, Topacio
+*/
